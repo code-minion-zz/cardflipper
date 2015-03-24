d0fbea5a354a2e16a31b5c347ce9a2c35a0af112
Kit <kit@cerebralfix.com>
2015-03-24 13:13:51 +1300

    saving so we dont lose stuff

--- CardFlipPrototype/Assets/CardAnimator.cs
+++ CardFlipPrototype/Assets/CardAnimator.cs
@@ -15,18 +15,18 @@ public class CardAnimator : MonoBehaviour {
         cardBack = transform.FindChild("Back").gameObject;
         cardFront.SetActive(false);
 
-        mySequence = DOTween.Sequence();
-        // Add a 1 second move tween only on the Y axis
-        mySequence.Append(transform.DOMoveY(2, 1));
-        // Add a 1 second rotation tween, using Join so it will start when the previous one starts
-        mySequence.Join(transform.DORotate(new Vector3(0, 135, 0), 1));
-        // Add a 1 second scale Y tween, using Append so it will start after the previously added tweens end
-        mySequence.Append(transform.DOScaleY(0.2f, 1));
-        // Add an X axis relative move tween that will start from the beginning of the Sequence
-        // and last for the whole Sequence duration
-        mySequence.Insert(0, transform.DOMoveX(4, mySequence.Duration()).SetRelative());
-        // Oh, and let's also make the whole Sequence loop backward and forward 4 times
-        mySequence.SetLoops(-1, LoopType.Yoyo);
+        //mySequence = DOTween.Sequence();
+        //// Add a 1 second move tween only on the Y axis
+        //mySequence.Append(transform.DOMoveY(2, 1));
+        //// Add a 1 second rotation tween, using Join so it will start when the previous one starts
+        //mySequence.Join(transform.DORotate(new Vector3(0, 135, 0), 1));
+        //// Add a 1 second scale Y tween, using Append so it will start after the previously added tweens end
+        //mySequence.Append(transform.DOScaleY(0.2f, 1));
+        //// Add an X axis relative move tween that will start from the beginning of the Sequence
+        //// and last for the whole Sequence duration
+        //mySequence.Insert(0, transform.DOMoveX(4, mySequence.Duration()).SetRelative());
+        //// Oh, and let's also make the whole Sequence loop backward and forward 4 times
+        //mySequence.SetLoops(-1, LoopType.Yoyo);
 	}
 	
 	// Update is called once per frame
